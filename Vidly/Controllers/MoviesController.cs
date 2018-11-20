86c304ad5ea7b6128137cace9bcdfee616a0365c
swrigh5 <swright2133@gmail.com>
2018-11-19 20:13:55 -0500

    11/19 - RandomMovieViewModel

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,21 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "House on Haunted Hill" };
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Stephen Wright"},
+                new Customer {Name = "Todd Buffington"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            
+
+            return View(viewModel);
            
         }
 
@@ -36,6 +51,8 @@ namespace Vidly.Controllers
             return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortby));            
         }
 
+
+        [Route("movies/released/{year:regex(^\\d{4}$)}/{month:range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, byte month)
         {
             return Content(year + "/" + month);
