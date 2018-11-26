ab12ce5feb5ee113f52fd33f48375ed30405a0a3
Stephen Wright <Stephen.Wright@amtrustgroup.com>
2018-11-26 17:31:32 -0500

    Customer Controller/_NavBar

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ////display all customers Index/Customers
+            //with all links for each customer taking them to their page @ Customers/Detail/(CustomerNumber)
+            //If there are no customers, display text stating so
+
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(d => d.Id == id);
+            //create a for loop instead
+            //foreach (var customer in GetCustomers())
+            //{
+            //    if (customer == null)
+            //        return HttpNotFound();
+            //    else
+            //    {
+            //        return View(customer);
+            //    }
+           
+            //}
+
+            return View(customer);
+       
+
+        }
+
+        private IEnumerable<customers> GetCustomers()
+        {
+            return new List<customers>
+            {
+                new customers { Id = 1, Name = "Stephen Wright" },
+                new customers { Id = 2, Name = "Todd Buffington" },
+                new customers { Id = 3, Name = "Fred Savage" }
+            };
+        }
+
+    }
+}
